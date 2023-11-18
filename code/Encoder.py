f57b31b8a256b21b70e03738cd0d83a4cc8808a0
Zhouxunzhe <1170543554@qq.com>
2023-11-18 14:58:01 -0800

    update encoder with pos_embedding, fix dim prob
    Signed-off-by: Zhouxunzhe <1170543554@qq.com>

--- code/Encoder.py
+++ code/Encoder.py
@@ -17,39 +17,22 @@ def deleteEncodingLayers(model, num_layers):
     return copyOfModel
 
 
-# Not used, just to demonstrate the structure
-vit_config = ViTConfig(hidden_size=768,
-                       num_hidden_layers=12,
-                       num_attention_heads=12,
-                       intermediate_size=3072,
-                       hidden_act='gelu',
-                       hidden_dropout_prob=0.0,
-                       attention_probs_dropout_prob=0.0,
-                       initializer_range=0.02,
-                       layer_norm_eps=1e-12,
-                       image_size=224,
-                       patch_size=16,
-                       num_channels=3,
-                       qkv_bias=True,
-                       encoder_stride=16,
-                       )
-
-
 class Lora_Encoder:
     def __init__(self, num_layers=6):
         self.processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
-        self.model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
-        self.config = LoraConfig(
+        self.vit_config = ViTConfig.from_pretrained("google/vit-base-patch16-224-in21k", output_hidden_states=True)
+        self.model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k', config=self.vit_config)
+        self.lora_config = LoraConfig(
             r=16,
             lora_alpha=16,
             target_modules=["query", "value"],
             lora_dropout=0.1,
             bias="none",
         )
-        lora_model = get_peft_model(self.model, self.config)
+        lora_model = get_peft_model(self.model, peft_config=self.lora_config)
         self.model = deleteEncodingLayers(lora_model, num_layers)
 
     def forward(self, image):
         inputs = self.processor(images=image, return_tensors="pt")
         outputs = self.model(**inputs)
-        return outputs.last_hidden_state
+        return outputs.last_hidden_state[:, 1:, :], self.model.embeddings.position_embeddings[:, 1:, :]
